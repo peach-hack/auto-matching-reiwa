231981398d6d623e7ac2e3a50f1361b7ee76c796
tsu-nera <fox10225fox@gmail.com>
2019-12-31 23:40:14 +0900

    ねるよ

--- engine/spiders/wakuwaku.py
+++ engine/spiders/wakuwaku.py
@@ -1,10 +1,13 @@
 import scrapy
 
+WAKUWAKU_DOMAIN = '550909.com'
+WAKUWAKU_URL = 'http://550909.com/m/'
+
 
 class WakuwakuSpider(scrapy.Spider):
     name = 'wakuwaku'
-    allowed_domains = ['550909.com']
-    start_urls = ['http://550909.com/']
+    allowed_domains = [WAKUWAKU_DOMAIN]
+    start_urls = [WAKUWAKU_URL]
 
     def parse(self, response):
         pass
