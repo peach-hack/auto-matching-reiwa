401dd89645f49733397020919fdfa6121d495e23
tsu-nera <fox10225fox@gmail.com>
2020-01-01 16:31:44 +0900

    closed #11

--- tasks.py
+++ tasks.py
@@ -1,19 +1,23 @@
 import invoke
 from invoke import task
 
-RAWDATA_WAKUWAKU = "./rawdata/wakuwaku.csv"
+RAWDATA_WAKUWAKU_TOKYO = "./rawdata/wakuwaku_tokyo.csv"
+RAWDATA_WAKUWAKU_KANAGAWA = "./rawdata/wakuwaku_kanagawa.csv"
 
 
-def create_crawl_wakuwaku_command(area, days):
+def create_crawl_wakuwaku_command(area, days, file_name):
     return "scrapy crawl -a area={} -a days={} wakuwaku -o {}".format(
-        area, days, RAWDATA_WAKUWAKU)
+        area, days, file_name)
 
 
 @task
 def crawl_wakuwaku(c, days):
     try:
-        invoke.run('rm {}'.format(RAWDATA_WAKUWAKU))
+        invoke.run('rm {}'.format(RAWDATA_WAKUWAKU_KANAGAWA))
+        invoke.run('rm {}'.format(RAWDATA_WAKUWAKU_TOKYO))
     except Exception:
         pass
-    invoke.run(create_crawl_wakuwaku_command("神奈川県", days))
-    invoke.run(create_crawl_wakuwaku_command("東京都", days))
+    invoke.run(
+        create_crawl_wakuwaku_command("神奈川県", days, RAWDATA_WAKUWAKU_KANAGAWA))
+    invoke.run(
+        create_crawl_wakuwaku_command("東京都", days, RAWDATA_WAKUWAKU_TOKYO))
