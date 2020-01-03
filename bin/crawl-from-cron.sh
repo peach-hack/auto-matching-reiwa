d6f8f9eb859861dfd609ff7cc3e05405bd6c3a0d
tsu-nera <fox10225fox@gmail.com>
2020-01-03 21:24:02 +0900

    cron-script

---
+++ bin/crawl-from-cron.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate auto-matching
+
+cd /home/tsu-nera/repo/auto-matching-reiwa
+
+scrapy crawl -a area="東京都" -a days=1 happymail --nolog
+scrapy crawl -a area="神奈川県" -a days=1 happymail --nolog
+scrapy crawl -a area="東京都" -a days=1 wakuwaku --nolog
+scrapy crawl -a area="神奈川県" -a days=1 wakuwaku --nolog
+scrapy crawl -a area="東京都" -a days=1 pcmax --nolog
+scrapy crawl -a area="神奈川県" -a days=1 pcmax --nolog
+scrapy crawl -a area="東京都" -a days=1 ikukuru --nolog
+scrapy crawl -a area="神奈川県" -a days=1 ikukuru --nolog
