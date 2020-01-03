84d84c2de5f530b356baeb1e28b0233e418a7e5d
tsu-nera <fox10225fox@gmail.com>
2020-01-04 01:55:08 +0900

    minorfix

--- sql/select_all.sql
+++ sql/select_all.sql
@@ -1,7 +1,7 @@
 USE auto_matching;
 
 SELECT 
-    title, name, age, posted_at, site, genre, prefecture, city, id, url, image_url, profile_id
+    title, name, age, posted_at, site, genre, prefecture, city, id, url, created_at, updated_at, image_url, profile_id
 FROM
     posts
 ORDER BY posted_at DESC
