a635a8ff50741f7baa509e3c35ef4fc09f8aa7b3
tsu-nera <fox10225fox@gmail.com>
2020-01-04 04:01:42 +0900

    NG word強化

--- sql/sakura_analysis.sql
+++ sql/sakura_analysis.sql
@@ -15,12 +15,22 @@ SELECT
 FROM
     posts
 WHERE
-    NOT EXISTS( SELECT 
+    LENGTH(title) < 64
+    AND LENGTH(name) < 20
+        AND NOT EXISTS( SELECT 
             word
         FROM
             ng_words
         WHERE
             title LIKE CONCAT('%', word, '%'))
+        AND name NOT LIKE '%女装%'
+        AND name NOT LIKE '%NH%'
+        AND name NOT LIKE '%◎%'
+        AND name NOT LIKE '%★%'
+        AND name NOT LIKE '%☆%'
+        AND age NOT LIKE '%40%'
+        AND age NOT LIKE '%50%'
+        AND age NOT LIKE '%60%'
 ORDER BY posted_at DESC
 LIMIT 10000
 ;
