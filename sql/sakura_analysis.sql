e9e8a2e975fb69831e260612af51a47590089608
tsu-nera <fox10225fox@gmail.com>
2020-01-04 14:20:34 +0900

    add comment

--- sql/sakura_analysis.sql
+++ sql/sakura_analysis.sql
@@ -14,29 +14,39 @@ SELECT
 FROM posts as p
 WHERE
   -- タイトルが長すぎる投稿は除外
-  LENGTH(title) < 64 -- 名前が長すぎる投稿は除外
-  AND LENGTH(name) < 20 -- 年齢が対象外の人は除外
+  LENGTH(title) < 64 --
+  --
+  -- 名前が長すぎる投稿は除外
+  AND LENGTH(name) < 20 --
+  --
+  -- 年齢が対象外の人は除外
   AND NOT EXISTS(
     SELECT
       age
     FROM ng_ages
     WHERE
       p.age LIKE CONCAT(age, '%')
-  ) -- 名前にNGワードが含まれている投稿を除外
+  ) --
+  --
+  -- 名前にNGワードが含まれている投稿を除外
   AND NOT EXISTS(
     SELECT
       name
     FROM ng_names
     WHERE
       p.name LIKE CONCAT('%', name, '%')
-  ) -- タイトルにNGワードが含まれている投稿を除外
+  ) --
+  --
+  -- タイトルにNGワードが含まれている投稿を除外
   AND NOT EXISTS(
     SELECT
       word
     FROM ng_words as ng
     WHERE
       p.title LIKE CONCAT('%', word, '%')
-  ) -- 投稿日時順に並べ替え
+  ) --
+  --
+  -- 投稿日時順に並べ替え
 ORDER BY
   posted_at DESC
 LIMIT
