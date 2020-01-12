3ccec1618be89b30e33617f4ef6b8e634f15d0b7
tsu-nera <fox10225fox@gmail.com>
2020-01-12 20:52:22 +0900

    closed #53

--- engine/spiders/ikukuru.py
+++ engine/spiders/ikukuru.py
@@ -123,7 +123,7 @@ class IkukuruSpider(scrapy.Spider):
             except Exception:
                 posted_at = datetime.datetime.strptime(posted_at_str,
                                                        '%Y/%m/%d %H:%M')
-            if now.month == 1 and posted_at.month == 12:
+            if posted_at.month > now.month:
                 posted_at = posted_at.replace(year=now.year - 1)
             else:
                 posted_at = posted_at.replace(year=now.year)
